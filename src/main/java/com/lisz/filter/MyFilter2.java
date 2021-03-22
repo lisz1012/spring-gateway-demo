565c4173e027d9d1453560c6224b0892341f3ec5
lisz1012 <lisz1012@163.com>
2021-03-22 15:48:01 -0700

    添加笔记

--- src/main/java/com/lisz/filter/MyFilter2.java
+++ src/main/java/com/lisz/filter/MyFilter2.java
@@ -14,30 +14,18 @@ import reactor.core.publisher.Mono;
 import java.util.List;
 
 @Component
-public class MyFilter implements Ordered, GlobalFilter {
+public class MyFilter2 implements Ordered, GlobalFilter {
 	// 做各种限流、鉴权、校验，对于Cookie、tokens等。所有的Filter都可以以组件的形式嵌入到网关的接入层里
 	// 跟JWT、Spring Security整合做鉴权，都是从exchange中拿到request，然后再拿到Headers、Cookie
 	@Override
 	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-		System.out.println(111);
-		ServerHttpRequest request = exchange.getRequest();
-		MultiValueMap<String, String> queryParams = request.getQueryParams();
-		List<String> list = queryParams.get("id");  // 这里也可以get("token);做权限认证
-		if (list == null || list.isEmpty()) {
-			System.out.println("ID is missing");
-//			exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
-//			return exchange.getResponse().setComplete();
-
-			DataBuffer wrap = exchange.getResponse().bufferFactory().wrap("ID is missing!".getBytes());
-			exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
-			return exchange.getResponse().writeWith(Mono.just(wrap));
-		}
+		System.out.println("222");
 		return chain.filter(exchange);
 	}
 
 	@Override
 	public int getOrder() {
 		// 数字越小越先进入这个Filter，0表示不设置优先级，所有的Filter是同一优先级
-		return 0;
+		return 100; //低优先级
 	}
 }
